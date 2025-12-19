1ef9bc8581b325d0ca61f1be9bba6175d980dda1
erfan <eesfandiar5@gmail.com>
2025-12-19 20:56:42 +0330

    implementation of booking feature

--- src/models.py
+++ src/models.py
@@ -7,6 +7,9 @@ from langchain.agents import create_agent
 from langgraph.checkpoint.sqlite import SqliteSaver
 from langchain.tools import tool
 from datetime import datetime
+from langchain_community.utilities import SQLDatabase
+from langgraph.runtime import get_runtime
+from dataclasses import dataclass
 
 load_dotenv()
 
@@ -16,7 +19,7 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_API_BASE = os.getenv("OPENAI_API_BASE")
 OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
 OLLAMA_HOST = os.getenv("OLLAMA_HOST", "http://localhost:11434")
-
+db = SQLDatabase.from_uri("sqlite:///appointments.db")
 
 # System prompt
 PROMPT = """You are an experienced English teacher.
@@ -36,6 +39,9 @@ Your job is:
 - Keep track of common mistakes and focus on them
 - Build on previous topics and exercises
 -Use tools to answer about time and date
+-If user asks about appointments, use the database tool to query the appointments database
+-If user wants to schedule an appointment, guide them through the process and use the database tool to add the appointment
+-If the time that user wants is not available, suggest alternative times
 
 Rules:
 - Always respond in English
@@ -51,6 +57,9 @@ checkpointer = SqliteSaver(conn)
 
 
 # Setup models
+@dataclass
+class RuntimeContext:
+    db: SQLDatabase
 
 @tool
 def get_current_time() -> str:
@@ -60,39 +69,51 @@ def get_current_time() -> str:
     """
     now = datetime.now()
     return now.strftime("%Y-%m-%d %H:%M:%S")
+@tool
+def execute_sql(query: str) -> str:
+    """Execute a SQLite command and return results."""
+    runtime = get_runtime(RuntimeContext)
+    db = runtime.context.db
+
+    try:
+        return db.run(query)
+    except Exception as e:
+        return f"Error: {e}"
+
 
 model_openai = ChatOpenAI(
     openai_api_key=OPENAI_API_KEY,
     openai_api_base=OPENAI_API_BASE,
     temperature=0,
-    streaming=True,
+    
 )
 
 model_ollama = ChatOllama(
     model=OLLAMA_MODEL,
     host=OLLAMA_HOST,
-    stream=True,
     temperature=0,
 )
 
 # Create agents with persistent memory
 agent_openai = create_agent(
     model=model_openai,
-    tools=[get_current_time],
+    tools=[get_current_time,execute_sql],
     system_prompt=PROMPT,
     checkpointer=checkpointer,
+    context_schema=RuntimeContext
 )
 
 agent_ollama = create_agent(
     model=model_ollama,
-    tools=[],
+    tools=[get_current_time, execute_sql],
     system_prompt=PROMPT,
     checkpointer=checkpointer,
+    context_schema=RuntimeContext
 )
 
 
 # Thread configuration
-THREAD_ID = "session1"
+THREAD_ID = "session5"
 CFG = {"configurable": {"thread_id": THREAD_ID}}
 
 
@@ -103,7 +124,8 @@ def generate_openai(prompt: str, model_name: str = "gpt-4o"):
         model_openai.model_name = model_name
         res = agent_openai.invoke(
             {"messages": [{"role": "user", "content": prompt}]},
-            config=CFG
+            config=CFG,
+            context=RuntimeContext(db=db)
         )
         return res["messages"][-1].content
     except Exception as e:
@@ -115,7 +137,8 @@ def generate_ollama(prompt: str, model_name: str = None):
         model_ollama.model = OLLAMA_MODEL
         res = agent_ollama.invoke(
             {"messages": [{"role": "user", "content": prompt}]},
-            config=CFG
+            config=CFG,
+            context=RuntimeContext(db=db)
         )
         return res["messages"][-1].content
     except Exception as e:
